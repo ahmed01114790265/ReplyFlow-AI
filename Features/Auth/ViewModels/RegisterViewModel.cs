034c79c78f37071ca7831bf8a8f2456bacbc48ec
ahmed01114790265 <01114790265ahmed.com>
2026-06-25 01:28:03 +0300

    create entity /config/enum/command/validation/viewmodel  only

---
+++ Features/Auth/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ReplyFlow.Features.Auth.ViewModels
+{
+
+    public sealed class RegisterViewModel
+    {
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
